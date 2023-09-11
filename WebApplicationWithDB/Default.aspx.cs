d6243bf16a81e3988c35a3cbb967b796b8a7c7dd
Administrator <Administrator@PF1NEZF1>
2023-09-11 09:52:11 +0200

    Add project files.

---
+++ WebApplicationWithDB/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplicationWithDB
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var connentionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connentionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT*from person", conn);
+                cmd.CommandType = CommandType.Text;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                dt.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+    }
+}
